6e4406007a7ae759d7857cc8402223bb5246942d
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-07-20 20:00:34 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Table, Row, Rows } from "react-native-table-component";
 
 export default function App() {
+  const tableHead = ["Id", "Empleado", "Detalle"];
+  const tableData = [
+    ["1", "Juan Perez", botonDetalle()],
+    ["2", "Pedro Lopez", botonDetalle()],
+    ["3", "Luis Nazar", botonDetalle()],
+    ["4", "Camilo Perez", botonDetalle()],
+    ["5", "Miguel Gonzalez", botonDetalle()],
+  ];
+
+  function botonDetalle() {
+    return (
+      <TouchableOpacity style={styles.detalleBoton}>
+        <Text style={styles.detalleTexto}>Hola</Text>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+        <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+        <Rows data={tableData} textStyle={styles.text} />
+      </Table>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
+  head: { height: 40, backgroundColor: "#f1f8ff" },
+  text: { margin: 6 },
+  detalleBoton: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  detalleTexto: {
+    color: "#123",
+    fontWeight: "bold",
+    fontSize: 17,
+    textAlign: "center",
   },
 });
